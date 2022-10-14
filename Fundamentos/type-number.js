c8c8d3ebf4b44f60d28eda380be59cbdebdd53e5
Javoso <lucasqueirozqxd@gmail.com>
2022-10-14 00:26:09 -0300

    Exemplos da Aula 1 do curso de Fundamentos do Javascript

---
+++ Fundamentos/type-number.js
@@ -0,0 +1,24 @@
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const soma=(primeiroNumero*segundoNumero)
+
+//ponto Flutuante
+
+const numeroPontoFlutuante = 0.33;
+const pontoFlutuanteSemZero = .33;
+
+const novaOperacao = primeiroNumero/segundoNumero;
+
+console.log(soma);
+
+console.log(numeroPontoFlutuante);
+console.log(pontoFlutuanteSemZero);
+
+console.log(novaOperacao);
+
+// NaN -> Not a Number(Não é um numero)
+
+const alura = "Alura";~
+
+console.log(alura * numeroPontoFlutuante);
---
+++ Fundamentos/type-string.js
@@ -0,0 +1,19 @@
+const texto1 ="Olá, mundo!"
+const texto2 ="Olá, mundo!"
+const senha  ="senhaSuper Segura 456!";
+const stringDeNumeros ="1324354";
+
+const citacao =" Meu nome é: ";
+const meuNome ="Lucas";
+
+console.log(citacao + meuNome);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
