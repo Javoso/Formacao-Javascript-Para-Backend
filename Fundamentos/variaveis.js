90feb802bd38309cdb4431276136cab28b9ff9d8
Javoso <lucasqueirozqxd@gmail.com>
2022-10-18 23:45:41 -0300

    Codigos var e conversores

---
+++ Fundamentos/variaveis.js
@@ -0,0 +1,41 @@
+//var
+
+var altura =5;
+var comprimento = 7;
+
+area = altura*comprimento;
+
+console.log(area);
+var area;
+
+//let
+
+let forma2 = "retângulo";
+let altura2 = 5;
+let comprimento2 = 7;
+let area2;
+
+
+if (forma2 === "retângulo") {
+    area2 = altura2 * comprimento2;
+} else {
+   area2 = (altura2 * comprimento2) / 2
+}
+console.log(area2);
+
+
+//const
+
+const forma3 = "triângulo";
+const altura3 = 5;
+const comprimento3 = 7;
+let area3;
+
+if (forma3 === "quadrado") {
+    area3 = altura3 * comprimento3;
+} else {
+   area3 = (altura3 * comprimento3) / 2
+}
+
+console.log(area3);
+
